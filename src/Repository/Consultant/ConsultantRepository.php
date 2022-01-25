79c7b9ffca5cea32fb83740dfda867a104434cd2
BASAK Semih <semihbasak25@gmail.com>
2022-01-25 21:16:30 +0100

    Consultant Entity & Repository

---
+++ src/Repository/Consultant/ConsultantRepository.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository\Consultant;
+
+use App\Entity\Consultant\Consultant;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
+
+/**
+ * @method Consultant|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Consultant|null findOneBy(array $criteria, array $orderBy = null)
+ * @method                 findAll()                array<int, Consultant>
+ * @method                 findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null) array<array-key, Consultant>
+ *
+ * @template T
+ *
+ * @extends ServiceEntityRepository<Consultant>
+ */
+final class ConsultantRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Consultant::class);
+    }
+
+    /**
+     * Used to upgrade (rehash) the user's password automatically over time.
+     */
+    public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
+    {
+        if (!$user instanceof Consultant) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', \get_class($user)));
+        }
+
+        $user->setPassword($newHashedPassword);
+        $this->_em->persist($user);
+        $this->_em->flush();
+    }
+}
