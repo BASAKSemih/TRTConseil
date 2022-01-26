4cc793a5069a5c8e1e414968696b31ab5889a374
BASAK Semih <semihbasak25@gmail.com>
2022-01-26 16:06:27 +0100

    make fix

--- src/Controller/Consultant/Security/ConsultantRecruiterController.php
+++ src/Controller/Consultant/Security/ConsultantRecruiterController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Controller\Consultant\Security;
 
-use App\Repository\Candidate\CandidateRepository;
 use App\Repository\Recruiter\RecruiterRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
