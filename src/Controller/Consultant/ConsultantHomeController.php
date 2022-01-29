78bbb70867a936f71cdf4e1fb29dad100f08098d
BASAK Semih <semihbasak25@gmail.com>
2022-01-29 11:55:28 +0100

    Consultant can see and confirm job

--- src/Controller/Consultant/ConsultantHomeController.php
+++ src/Controller/Consultant/ConsultantHomeController.php
@@ -7,7 +7,9 @@ namespace App\Controller\Consultant;
 use App\Repository\Candidate\CandidateRepository;
 use App\Repository\Recruiter\JobOfferRepository;
 use App\Repository\Recruiter\RecruiterRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,7 +19,8 @@ final class ConsultantHomeController extends AbstractController
     public function __construct(
         protected CandidateRepository $candidateRepository,
         protected RecruiterRepository $recruiterRepository,
-        protected JobOfferRepository $jobOfferRepository
+        protected JobOfferRepository $jobOfferRepository,
+        protected EntityManagerInterface $entityManager
     ) {
     }
 
@@ -39,13 +42,51 @@ final class ConsultantHomeController extends AbstractController
     {
         $candidates = $this->candidateRepository->findByIsVerified(false);
         $recruiters = $this->recruiterRepository->findByIsVerified(false);
-        $jobOffers = $this->jobOfferRepository->findByIsVerified(false);
 
         return $this->render('consultant/showAll.html.twig', [
             'candidates' => $candidates,
             'recruiters' => $recruiters,
+        ]);
+    }
+
+    #[Route('/espace-consultant/liste-des-emploies-non-verifier', name: 'show_all_jobOffer')]
+    public function showAllNoVerifiedJob(): Response
+    {
+        $jobOffers = $this->jobOfferRepository->findByIsVerified(false);
+
+        return $this->render('consultant/showAll.html.twig', [
             'jobOffers' => $jobOffers,
         ]);
+    }
+
+    #[Route('/espace-consultant/voir-le-poste/{idJobOffer}', name: 'show_jobOffer')]
+    public function showJobOffer(int $idJobOffer): Response
+    {
+        $jobOffer = $this->jobOfferRepository->findOneById($idJobOffer);
+        if (!$jobOffer) {
+            $this->addFlash('warning', "Cette offre emploi n'existe pas n'existe pas");
+
+            return $this->redirectToRoute('consultant_show_all');
+        }
+        return $this->render('consultant/JobOffer/show.html.twig', [
+            'jobOffer' => $jobOffer
+        ]);
+    }
+
+    #[Route('/espace-consultant/confirmer-le-poste/{idJobOffer}', name: 'confirm_jobOffer')]
+    public function confirmJobOffer(int $idJobOffer): RedirectResponse
+    {
+        $jobOffer = $this->jobOfferRepository->findOneById($idJobOffer);
+        if (!$jobOffer) {
+            $this->addFlash('warning', "Cette offre emploi n'existe pas n'existe pas");
+
+            return $this->redirectToRoute('consultant_show_all_jobOffer');
+        }
+        $jobOffer->setIsVerified(true);
+        $this->entityManager->flush();
+        $this->addFlash('success', "l'offre emploie à bien été vérifier");
+
+        return $this->redirectToRoute('consultant_show_all_jobOffer');
     }
 
 }
