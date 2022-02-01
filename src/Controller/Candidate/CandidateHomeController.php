fefc756c05ade3b7bf349a88cce2a7f75e007c74
BASAK Semih <semihbasak25@gmail.com>
2022-02-01 15:29:50 +0100

    candidat can see joboffer verified

--- src/Controller/Candidate/CandidateHomeController.php
+++ src/Controller/Candidate/CandidateHomeController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Controller\Candidate;
 
+use App\Repository\Recruiter\JobOfferRepository;
+use phpDocumentor\Reflection\Types\True_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,16 +13,23 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route(name: 'candidate_')]
 final class CandidateHomeController extends AbstractController
 {
+    public function __construct(protected JobOfferRepository $jobOfferRepository)
+    {
+    }
+
     #[Route('/espace-candidat', name: 'homePage')]
     public function homePage(): Response
     {
         $user = $this->getUser();
+        $jobOffers = $this->jobOfferRepository->findByIsVerified(true);
         if (!$user) {
             $this->addFlash('error', 'Erreur');
 
             return $this->redirectToRoute('homePage');
         }
 
-        return $this->render('candidate/home.html.twig');
+        return $this->render('candidate/home.html.twig', [
+            'jobOffers' => $jobOffers
+        ]);
     }
 }
