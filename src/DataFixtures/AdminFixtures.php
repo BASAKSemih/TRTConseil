ec1375d93a43c7b75007db4c40c27ebcc4f4edd1
BASAK Semih <semihbasak25@gmail.com>
2022-02-09 14:43:24 +0100

    admin migration db

---
+++ src/DataFixtures/AdminFixtures.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+final class AdminFixtures extends Fixture
+{
+
+    public function load(ObjectManager $manager): void
+    {
+
+    }
+}
